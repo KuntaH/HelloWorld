e2afe994e853173563a6dcacbdafc72f01b48dd6
Kunta Houen <kuntahouen@gmail.com>
2020-08-17 15:28:37 +0200

    Endringer i Main

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,9 @@ public class HelloWorld {
         for(int i =1;i<=10;i++){
             System.out.println(i);
         }
+        System.out.println("Petra teller til 20.");
+        for(int i =11;i<=20;i++){
+            System.out.println(i);
+        }
     }
 }
